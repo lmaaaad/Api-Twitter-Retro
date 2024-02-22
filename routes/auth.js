5899c41d0b8e2067efbc2a3cf0cac6182f315292
Alami-Boudkhil <b.alami@esi-sba.dz>
2024-02-22 14:13:46 +0100

    initialisation and auth implemenataion

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express";
+import Router from "express";
+
+import {login} from "../controllers/auth.js";
+
+const router = express.Router(); 
+router.post("/login", login);
+
+export default router;
+
+
